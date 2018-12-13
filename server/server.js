8ac5c081f95fa8eff5964c1fdf3358247cc67629
wandyirawan <wandy.irawan17@gmail.com>
2018-12-13 10:54:46 +0700

    setting test testing db

--- server/server.js
+++ server/server.js
@@ -1,4 +1,15 @@
+const env = process.env.NODE_ENV || 'development';
+console.log(env);
+
+if (env === 'development') {
+  process.env.PORT = 4000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if (env === 'test') {
+  process.env.PORT = 4000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}
 require('./db/mongoose');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectId } = require('mongodb');
